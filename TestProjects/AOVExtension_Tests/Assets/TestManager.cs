90e28f25e7e272f47db3ad4b08aaa8105d321b23
Victor Lui S.C <victorl@unity3d.com>
2019-11-24 17:38:34 +0900

    Test project now takes render pass of light decomposition passes.

--- TestProjects/AOVExtension_Tests/Assets/TestManager.cs
+++ TestProjects/AOVExtension_Tests/Assets/TestManager.cs
@@ -45,6 +45,8 @@ public class TestManager : MonoBehaviour
     [SerializeField] RenderPassToggle[] renderPassTogglesAOV;
     [SerializeField] RenderResolution renderpassOutputResolution;
 
+    [SerializeField] Material refractionAOVSimplifyMat;
+
     // Initialized at Start and never changed
     UnityAction handleButtonPressed;
 
@@ -125,10 +127,13 @@ public class TestManager : MonoBehaviour
 
         Texture2D screenShot = new Texture2D(width, height, TextureFormat.ARGB32, false);
 
+        bool hasRefractionPassRequest = false;
+        bool hasTransmittancePassRequest = false;
         for (int II = 0; II < toggles.Length; II++)
         {
             if (toggles[II].toggle.isOn)
             {
+                bool leaveWorkForLater = false;
                 AOVRequest aovRequest = new AOVRequest(AOVRequest.@default).SetLightFilter(DebugLightFilterMode.None);
 
                 switch (toggles[II].type)
@@ -139,38 +144,137 @@ public class TestManager : MonoBehaviour
                     case RP_TYPE.NORMAL:
                         aovRequest = aovRequest.SetFullscreenOutput(MaterialSharedProperty.Normal);
                         break;
+                    case RP_TYPE.DIRECT_DIFFUSE:
+                        aovRequest = aovRequest.SetFullscreenOutput(LightingProperty.DirectDiffuseOnly);
+                        break;
+                    case RP_TYPE.DIRECT_SPECULAR:
+                        aovRequest = aovRequest.SetFullscreenOutput(LightingProperty.DirectSpecularOnly);
+                        break;
+                    case RP_TYPE.INDIRECT_DIFFUSE:
+                        aovRequest = aovRequest.SetFullscreenOutput(LightingProperty.IndirectDiffuseOnly);
+                        break;
+                    case RP_TYPE.REFLECT:
+                        aovRequest = aovRequest.SetFullscreenOutput(LightingProperty.ReflectionOnly);
+                        break;
+                    case RP_TYPE.REFRACT:
+                        leaveWorkForLater = true;
+                        hasRefractionPassRequest = true; // Will deal with this case specially
+                        break;
+                    case RP_TYPE.TRANSMITTANCE:
+                        leaveWorkForLater = true;
+                        hasTransmittancePassRequest = true; // Will deal with this case specially
+                        break;
+                    case RP_TYPE.EMISSIVE:
+                        aovRequest = aovRequest.SetFullscreenOutput(LightingProperty.EmissiveOnly);
+                        break;
                     default:
                         break;
                 }
 
-                aovRequestBuilder.Add(
+                if (!leaveWorkForLater)
+                {
+                    aovRequestBuilder.Add(
                     aovRequest,
                     allocateAovStoreBuffer,
                     null,
                     requestOutputBuffer,
                     doNothing);
 
-                AOVRequestDataCollection totalAOVRequests = aovRequestBuilder.Build();
+                    camData.SetAOVRequests(aovRequestBuilder.Build());
 
-                camData.SetAOVRequests(totalAOVRequests);
+                    // Finished for this frame, wait for the GPU to finish rendering...
+                    yield return new WaitForEndOfFrame();
 
-                // Finished for this frame, wait for the GPU to finish rendering...
-                yield return new WaitForEndOfFrame();
+                    // At this point rendering of last frame to rtHandle is finished, read this back to file system
+                    {
+                        RenderTexture oldRT = RenderTexture.active;
+                        RenderTexture.active = aovStoreBuffer;
+                        screenShot.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+                        RenderTexture.active = oldRT;
 
-                // At this point rendering of last frame to rtHandle is finished, read this back to file system
-                {
-                    RenderTexture oldRT = RenderTexture.active;
-                    RenderTexture.active = aovStoreBuffer;
-                    screenShot.ReadPixels(new Rect(0, 0, width, height), 0, 0);
-                    RenderTexture.active = oldRT;
+                        string aovFileName = photoPath + fileName + "_" + toggles[II].type.ToString() + ".png";
+                        File.WriteAllBytes(aovFileName, screenShot.EncodeToPNG());
+                    }
 
-                    byte[] bytes = screenShot.EncodeToPNG();
-                    string aovFileName = photoPath + fileName + "_" + toggles[II].type.ToString() + ".png";
-                    File.WriteAllBytes(aovFileName, bytes);
+                    // Advance to build next AOV Request if unnessesary in the same frame
                 }
-
-                // Advance to build next AOV Request if unnessesary in the same frame 
             }
+        }
+
+        if (hasTransmittancePassRequest || hasRefractionPassRequest)
+        {
+            AOVRequest aovRequest = new AOVRequest(AOVRequest.@default).SetLightFilter(DebugLightFilterMode.None);
+            aovRequest = aovRequest.SetFullscreenOutput(LightingProperty.Transmittance);
+
+            aovRequestBuilder.Add(
+                aovRequest,
+                allocateAovStoreBuffer,
+                null,
+                requestOutputBuffer,
+                doNothing);
+
+            camData.SetAOVRequests(aovRequestBuilder.Build());
+
+            yield return new WaitForEndOfFrame();
+
+            if (hasTransmittancePassRequest)
+            {
+                RenderTexture oldRT = RenderTexture.active;
+                RenderTexture.active = aovStoreBuffer;
+                screenShot.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+                RenderTexture.active = oldRT;
+
+                string aovFileName = photoPath + fileName + "_" + RP_TYPE.TRANSMITTANCE.ToString() + ".png";
+                File.WriteAllBytes(aovFileName, screenShot.EncodeToPNG());
+            }
+        }
+
+        if (hasRefractionPassRequest)
+        {
+            // At this point, aovStoreBuffer should contain the transmittance AOV
+
+            RTHandle extraAovStoreBuffer = RTHandles.Alloc(width, height);
+
+            AOVRequestBufferAllocator allocateExtraAovStoreBuffer = (bufferId) =>
+            {
+                return extraAovStoreBuffer;
+            };
+
+            AOVRequest aovRequest = new AOVRequest(AOVRequest.@default).SetLightFilter(DebugLightFilterMode.None);
+            aovRequest = aovRequest.SetFullscreenOutput(LightingProperty.RefractionOnly);
+
+            aovRequestBuilder.Add(
+                aovRequest,
+                allocateExtraAovStoreBuffer,
+                null,
+                requestOutputBuffer,
+                doNothing);
+
+            camData.SetAOVRequests(aovRequestBuilder.Build());
+
+            yield return new WaitForEndOfFrame();
+
+            // We need to combine the transmittance and refraction pass
+            // Note now camTargetRT is no longer in use, let's use it as destination RT
+
+            targetCamera.enabled = false;
+
+            refractionAOVSimplifyMat.SetTexture("_TransmittanceMap", aovStoreBuffer);
+            Graphics.Blit(extraAovStoreBuffer, camTargetRT, refractionAOVSimplifyMat);
+
+            yield return new WaitForEndOfFrame();
+
+            RTHandles.Release(extraAovStoreBuffer);
+
+            RenderTexture oldRT = RenderTexture.active;
+            RenderTexture.active = camTargetRT;
+            screenShot.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+            RenderTexture.active = oldRT;
+
+            targetCamera.enabled = true;
+
+            string aovFileName = photoPath + fileName + "_" + RP_TYPE.REFRACT.ToString() + ".png";
+            File.WriteAllBytes(aovFileName, screenShot.EncodeToPNG());
         }
 
         Destroy(screenShot);
---
+++ TestProjects/AOVExtension_Tests/Assets/_Assets/Shaders/RefractionAOVSimplify.shader
@@ -0,0 +1,54 @@
+﻿Shader "Hidden/RefractionAOVSimplify"
+{
+    Properties
+    {
+        _MainTex ("Main Input", 2D) = "white" {}
+		_TransmittanceMap("Transmittance Map", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+			sampler2D _TransmittanceMap;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				fixed4 col = tex2D(_MainTex, i.uv);
+				fixed4 transmittance = tex2D(_TransmittanceMap, i.uv);
+
+				// Black out refraction AOV when transmittance is super small
+				col.xyz = col.xyz * step(0.001, transmittance.x + transmittance.y + transmittance.z);
+				return col;
+            }
+
+            ENDCG
+        }
+    }
+}
